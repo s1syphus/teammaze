31447385eafcaa8d6fc23c9d3436ca656cc24ffb
migabreu <migabreu@umich.edu>
2013-10-13 02:19:51 -0400

    added maze and roadblock activities
    *Added Maze GUI layout and java file
    *Added RoadBlock GUI layout and java file
    *Changed the layout of the main page from relative to linear
    *Added the function to get into the maze gui into main_activity.java
    *Added some string values

--- gen/teammaize/android/com/R.java
+++ gen/teammaize/android/com/R.java
@@ -28,14 +28,28 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_maze_gui=0x7f030001;
+        public static final int activity_road_block=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int maze_gui=0x7f070001;
+        public static final int road_block=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int choice_four=0x7f050007;
+        public static final int choice_one=0x7f050004;
+        public static final int choice_three=0x7f050006;
+        public static final int choice_two=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int question_here=0x7f050008;
+        public static final int roadblock_test=0x7f05000b;
+        public static final int team_maize=0x7f05000a;
+        public static final int test=0x7f050009;
+        public static final int title_activity_maze_gui=0x7f05000c;
+        public static final int title_activity_road_block=0x7f050003;
     }
     public static final class style {
         /** 
