fb9d1aecd19a4f6cb5313b5af9e7a8403be1547c
Erika Jansen <emjansen@umich.edu>
2013-11-26 14:33:58 -0500

    Fixing crash with the up roadblock button

--- src/teammaize/android/com/RoadBlock.java
+++ src/teammaize/android/com/RoadBlock.java
@@ -91,7 +91,6 @@ public class RoadBlock extends ASKActivity {
 	 * Set up the {@link android.app.ActionBar}.
 	 */
 	private void setupActionBar() {
-
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 	}
@@ -103,23 +102,17 @@ public class RoadBlock extends ASKActivity {
 		return true;
 	}
 	
-	
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		// This ID represents the Home or Up button. In the case of this
+		// activity, the Up button is shown. Use NavUtils to allow users
+		// to navigate up one level in the application structure. For
+		// more details, see the Navigation pattern on Android Design:
+		//
+		// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+		
+		startActivity(new Intent(RoadBlock.this, MainActivity.class)); 
+        return true;
 	}
 	
 	//manages the closing of the activity
